5c8bc3f664cc35626a97abc1de29173df477d72a
Bobby Roe <bobby.roe@unifyconsulting.com>
2024-01-06 14:05:26 -0800

    udpate basic template

--- index.js
+++ index.js
@@ -1,25 +1,39 @@
-import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.118/build/three.module.js";
+import * as THREE from "three";
+import getLayer from "../libs/bobbyroe/getLayer.js";
 
 const w = window.innerWidth;
 const h = window.innerHeight;
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
 camera.position.z = 5;
-const renderer = new THREE.WebGLRenderer({ antiAlias: true });
+const renderer = new THREE.WebGLRenderer();
 renderer.setSize(w, h);
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+renderer.outputColorSpace = THREE.SRGBColorSpace;
 document.body.appendChild(renderer.domElement);
 
 const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0xFFFF00 });
+const material = new THREE.MeshStandardMaterial({
+  color: 0xffff00,
+});
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
 
-const box = new THREE.Mesh(geometry, material);
-scene.add(box);
+const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444);
+scene.add(hemiLight);
 
 function animate() {
   requestAnimationFrame(animate);
-  box.rotation.y += 0.02;
-  box.rotation.z += 0.01;
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.02;
   renderer.render(scene, camera);
 }
 
 animate();
+
+function handleWindowResize () {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+window.addEventListener('resize', handleWindowResize, false);
